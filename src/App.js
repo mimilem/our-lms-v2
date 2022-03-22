5353a89de45ff45585d57abefee01892a253d333
elsa manang <manangelsa07@gmail.com>
2022-03-22 14:29:24 +0100

    mise en place de la structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './layout/header';
 import Footer from './layout/footer';
+import { Route, Routes } from "react-router-dom";
+import Home from './views/home';
+import Campus from './views/campus';
+import Contact from './views/contact';
 
-function App() {
-  return (
-    <>
-      <Header />
 
-      <Footer />
-    </>
-  );
+class App extends Component {
+  render () {
+    return (
+      <>
+        <Header />
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/campus' element={<Campus />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/' element={<Home />} />
+        </Routes>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
