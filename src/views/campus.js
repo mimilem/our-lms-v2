19a4ccd1824a13e4f9d577bc5de0e06172c028af
elsa manang <manangelsa07@gmail.com>
2022-03-27 13:41:00 +0200

    campus

--- src/views/campus.js
+++ src/views/campus.js
@@ -159,29 +159,6 @@ class Campus extends Component {
                     </div>
                 </section>
                 
-                <section className="clients-section">
-                    <div className="auto-container">
-                        <div className="sec-title centered">
-                            <h2>Nors partenaires</h2>
-                            <div className="text">These leading universities are currently offering online degree <br/> courses on FutureLearn:</div>
-                        </div>
-                        
-                        <div className="sponsors-outer">
-                            <ul className="sponsors-carousel owl-carousel owl-theme">
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/1.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/2.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/3.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/4.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/5.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/1.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/2.png" alt=""/></a></figure></li>
-                                <li className="slide-item"><figure className="image-box"><a href="#"><img src="images/clients/3.png" alt=""/></a></figure></li>
-                            </ul>
-                        </div>
-                        
-                    </div>
-                </section>
-
                 <section className="contact-section">
                     <div className="pattern-layer" style={{BackgroundImage: "url(images/background/pattern-19.png)"}}></div>
                     <div className="pattern-layer-two" style={{BackgroundImage: "url(images/background/pattern-20.png)"}}></div>
