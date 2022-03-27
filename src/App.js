909a48f2cc568c546493da9abbb41f0e4455eb58
elsa manang <manangelsa07@gmail.com>
2022-03-27 13:40:37 +0200

    routage

--- src/App.js
+++ src/App.js
@@ -6,21 +6,32 @@ import { Route, Routes } from "react-router-dom";
 import Home from './views/home';
 import Campus from './views/campus';
 import Contact from './views/contact';
+import Event from './views/event';
+import Notice from './views/notice';
+import Fees from './views/fees';
+import Calender from './views/calender';
+import Library from './views/library';
 
 
 class App extends Component {
   render () {
     return (
-      <>
+      <div className="page-wrapper">
+        <div className="preloader"></div>
         <Header />
         <Routes>
           <Route path='/home' element={<Home />} />
           <Route path='/campus' element={<Campus />} />
-          <Route path='/contact' element={<Contact />} />
+          <Route path='/about' element={<Contact />} />
+          <Route path='/event' element={<Event />} />
+          <Route path='/notice' element={<Notice />} />
+          <Route path='/fees' element={<Fees />} />
+          <Route path='/calender' element={<Calender />} />
+          <Route path='/library' element={<Library />} />
           <Route path='/' element={<Home />} />
         </Routes>
         <Footer />
-      </>
+      </div>
     );
   }
 }
